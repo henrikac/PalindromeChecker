23bba7de7ebcb34e4c36346bb73f1093b5a0a22e
Henrik Abel Christensen <sensen1695@hotmail.com>
2018-11-03 14:48:05 +0100

    added is_palindrome_iter function

--- src/main.c
+++ src/main.c
@@ -7,11 +7,37 @@
  * Write a recursive function 'int is_palindrome_rec(char *str) that also checks if str is a palindrome.
 */
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef enum { false, true } bool;
 
+bool is_palindrome_iter(char *str);
+
 int main(void)
 {
+  int i;
+  char *words[] = { "regninger", "palindrome", "kajak" };
+
+  for (i = 0; i < 3; i++)
+  {
+    printf("\n%s %s a palindrome", words[i], is_palindrome_iter(words[i]) ? "is" : "is not");
+  }
+
   return EXIT_SUCCESS;
+}
+
+bool is_palindrome_iter(char *str)
+{
+  int i;
+  size_t len_str = strlen(str);
+
+  for (i = 0; i < len_str; i++)
+  {
+    if (str[i] != str[(len_str - 1) - i])
+      return false;
+  }
+
+  return true;
 }
