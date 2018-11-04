7991fec5973f8bf80fac4e6221ff27908f091d0b
Henrik Abel Christensen <sensen1695@hotmail.com>
2018-11-04 16:36:51 +0100

    added is_palindrome_rec function

--- src/main.c
+++ src/main.c
@@ -14,15 +14,16 @@
 typedef enum { false, true } bool;
 
 bool is_palindrome_iter(char *str);
+bool is_palindrome_rec(char *str);
 
 int main(void)
 {
   int i;
-  char *words[] = { "regninger", "palindrome", "kajak" };
+  char *words[] = { "regninger", "paliniilap", "kajak" };
 
   for (i = 0; i < 3; i++)
   {
-    printf("\n%s %s a palindrome", words[i], is_palindrome_iter(words[i]) ? "is" : "is not");
+    printf("\n%s %s a palindrome", words[i], is_palindrome_rec(words[i]) ? "is" : "is not");
   }
 
   return EXIT_SUCCESS;
@@ -34,10 +35,33 @@ bool is_palindrome_iter(char *str)
   size_t len_str = strlen(str);
 
   for (i = 0; i < len_str; i++)
-  {
     if (str[i] != str[(len_str - 1) - i])
       return false;
+
+  return true;
+}
+
+bool is_palindrome_rec(char *str)
+{
+  char *new_str = NULL;
+  size_t len_str = strlen(str);
+
+  if (str[0] != str[len_str - 1])
+    return false;
+  else if (len_str > 3)
+  {
+    new_str = (char*)calloc(len_str - 1, sizeof(char));
+    if (new_str == NULL)
+    {
+      printf("\nCouldn't allocate memory");
+      exit(EXIT_FAILURE);
+    }
+    strncpy(new_str, &str[1], len_str - 2);
+    return is_palindrome_rec(new_str);
   }
 
+  if (new_str)
+    free(new_str);
+  
   return true;
 }
